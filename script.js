257d80516ae9e32397a06aa715cfda19f60a92e7
afsona571 <afsonaa59@gmail.com>
2024-12-02 18:26:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//  for (let i = 0; i < 100; i++) {
+//      console.log({i});
+//  }
+ //for (let i = 0; i < 100; i++) {
+   // if (i % 2 == 0)
+   // console.log({i});}
+
+   let newar=[]
+  let array =[2,4,9,10,45,67,8,90]
+    for (let i = 0; i< array.length; i++){
+       
+      if(array[i] %2 === 0)
+        console.log(array[i]);
+      newar.push("juft son lik array"+  array[i])
+       
+    }
+  
